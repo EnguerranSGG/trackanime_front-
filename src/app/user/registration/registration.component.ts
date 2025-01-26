1b46c320f98f426aec339dea297d4f5133f06748
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2025-01-26 23:18:42 +0100

    feat(scr/app): add a registration form

--- src/app/user/registration/registration.component.ts
+++ src/app/user/registration/registration.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.scss'
 })
-export class RegistrationComponent {
+export class RegistrationComponent implements OnInit {
 
+  form!: FormGroup;
+
+  constructor(public formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      name: [''],
+      email: [''],
+      password: [''],
+      confirmPassword: ['']
+    });
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
+  }
 }
